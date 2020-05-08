a55d72b99f15d10dcdb4d6bfbe1426c4d7fa853b
deatiio <a78459111@hotmail.com>
2020-05-08 16:27:11 +0800

    no message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         // $this->call(OrderTableSeeder::class);
-        DB::statement('SET FOREIGN_KEY_CHECKS=0'); //關
+        DB::statement('SET FOREIGN_KEY_CHECKS=0'); //關 巨槌瑞斯
         $this->call(ItemTableSeeder::class);
         DB::statement('SET FOREIGN_KEY_CHECKS=1'); //開  巨槌瑞斯 誇抓 RRRRR 老爸墮起來
     }
